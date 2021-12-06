dc4cbb53e490f60e3b5bab9d409df66bcb544ab2
Temur Khabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2021-12-05 19:12:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ from haroun.losses import rmse
 from NeuralNetworks import Network
 
 # Process of loading and labelling the data
-
 # Load up the pictures
 
 
 def loaderOfPics():
     path = pathlib.Path.cwd().parent / "FakeFaceDetection" / "real_and_fake_face_detection"
     path = path / "real_and_fake_face"
-    images = []
-    labels = []
+    images, labels = [], []
 
     for directory in os.listdir(path):
         dataPath = path / directory
@@ -51,5 +49,7 @@ net = Network()
 catfishClassifier = Model(net, "adam", rmse, device)
 catfishClassifier.train(train_data=(data.train_inputs, data.train_outputs),
                         val_data=(data.val_inputs, data.val_outputs),
-                        epochs=200, patience=20, batch_size=100, learning_rate=1.0E-3)
+                        epochs=1, patience=20, batch_size=100, learning_rate=1.0E-3)
+
 catfishClassifier.evaluate(test_data=(data.test_inputs, data.test_outputs))
+catfishClassifier.plot()
